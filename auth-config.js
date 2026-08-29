e8b21052b8726c6e333ac3d511554d590e0b4408
buanamedia <media.agussiahaan@gmail.com>
2026-08-29 21:54:09 +0700

    Delete auth-config.js

--- auth-config.js
+++
@@ -1,4 +0,0 @@
-window.AUTH_CONFIG = {
-  USER_HASH: "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", // Hash SHA256 "admin"
-  PASS_HASH: "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9"  // Hash SHA256 "admin123"
-};
