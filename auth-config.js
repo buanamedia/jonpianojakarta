7684ead690071d4d8368ddc5a30525529728678d
buanamedia <media.agussiahaan@gmail.com>
2026-09-01 07:22:32 +0000

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -1,4 +1,11 @@
 window.AUTH_CONFIG = {
   USER_HASH: "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918",
-  PASS_HASH: "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9"
+  PASS_HASH: "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9",
+  
+  // Encoded URL Google Apps Script WebApp
+  SCRIPT_URL_ENCODED: "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J3UkY4RlRQUA==",
+  
+  // Default Whatsapp & Backsound Fallback
+  DEFAULT_WA_NUMBER: "082370958651",
+  DEFAULT_BACKSOUND_URL: "https://assets.mixkit.co/music/preview/mixkit-piano-reflections-22.mp3"
 };
