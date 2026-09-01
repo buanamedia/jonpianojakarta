45cbebeb26022d1c8db73331fb8520f377a57f75
buanamedia <media.agussiahaan@gmail.com>
2026-09-01 09:00:31 +0000

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -2,8 +2,8 @@ window.AUTH_CONFIG = {
   USER_HASH: "c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f",
   PASS_HASH: "385697cc5d929af58f05ead7e281d995c8f89f1f24e8f26cda58ae71de97ff09",
   
-  // Encoded URL Google Apps Script WebApp
-  SCRIPT_URL_ENCODED: "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J3UkY4RlRQUA==",
+  // Encoded URL Google Apps Script WebApp Baru (Berisi API Key Cloudinary & Fitur Delete)
+  SCRIPT_URL_ENCODED: "YUhSMGNITTZMeTl6WTNKcGNIUXVaMjl2ZDJ4bExtTnZiUzl0WVdOeWIzTXZjL0FGS2Z5Y2J4cXRCWFdwZWlXQlgybko1OFhQN0Z2YURCcWM5dTF4cF95WHlWOVU2Mm1ydlJ4WUZ6UFczSkpFem5xYjNxYjJsMldNZy9leGVj",
   
   // Default Whatsapp & Backsound Fallback
   DEFAULT_WA_NUMBER: "082370958651",
