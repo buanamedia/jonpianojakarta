eb6fd1da93a729303f5288e7e743c1c6adef1089
buanamedia <media.agussiahaan@gmail.com>
2026-09-03 07:00:58 +0000

    Update auth-config.js

--- auth-config.js
+++ auth-config.js
@@ -2,10 +2,16 @@ window.AUTH_CONFIG = {
   USER_HASH: "c1c224b03cd9bc7b6a86d77f5dace40191766c485cd55dc48caf9ac873335d6f",
   PASS_HASH: "385697cc5d929af58f05ead7e281d995c8f89f1f24e8f26cda58ae71de97ff09",
   
-  // Encoded URL Google Apps Script WebApp Baru (Berisi API Key Cloudinary & Fitur Delete)
-  SCRIPT_URL_ENCODED: "YUhSMGNITTZMeTl6WTNKcGNIUXVaMjl2ZDJ4bExtTnZiUzl0WVdOeWIzTXZjL0FGS2Z5Y2J4cXRCWFdwZWlXQlgybko1OFhQN0Z2YURCcWM5dTF4cF95WHlWOVU2Mm1ydlJ4WUZ6UFczSkpFem5xYjNxYjJsMldNZy9leGVj",
-  
-  // Default Whatsapp & Backsound Fallback
+  // KREDENSIAL FIREBASE PROYEK ANDA
+  FIREBASE_CONFIG: {
+    apiKey: "AIzaSy...",
+    authDomain: "jon-piano-jakarta.firebaseapp.com",
+    projectId: "jon-piano-jakarta",
+    storageBucket: "jon-piano-jakarta.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abcdef123456"
+  },
+
   DEFAULT_WA_NUMBER: "082370958651",
   DEFAULT_BACKSOUND_URL: "https://assets.mixkit.co/music/preview/mixkit-piano-reflections-22.mp3"
 };
